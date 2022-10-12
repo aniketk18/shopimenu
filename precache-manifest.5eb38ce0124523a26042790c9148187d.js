22822db44157c2f6e0dc397e8441cdae2a693e5a
Aniket Kumawat <anikethanu418@gmail.com>
2022-10-12 10:49:34 +0530

    Updates

---
+++ precache-manifest.5eb38ce0124523a26042790c9148187d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4af4330548f2f7ed1aa",
+    "url": "/shopimenu/static/css/main.71c70505.chunk.css"
+  },
+  {
+    "revision": "d4af4330548f2f7ed1aa",
+    "url": "/shopimenu/static/js/main.6a90be53.chunk.js"
+  },
+  {
+    "revision": "9b52ad1cc4cafa8d5ca3",
+    "url": "/shopimenu/static/js/runtime~main.a34b4407.js"
+  },
+  {
+    "revision": "0e1f722a0fe7da2c3cb7",
+    "url": "/shopimenu/static/css/2.99ab3d55.chunk.css"
+  },
+  {
+    "revision": "0e1f722a0fe7da2c3cb7",
+    "url": "/shopimenu/static/js/2.d0521b2a.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopimenu/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopimenu/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopimenu/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopimenu/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopimenu/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3aa446366ffd9d720645b0a1ed3cab3b",
+    "url": "/shopimenu/index.html"
+  }
+];
